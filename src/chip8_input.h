8cce8c92aa9a6b5260bbc7df7f81c7bc7d69b430
aimar <aimar.a@opendeusto.es>
2025-03-06 22:21:41 +0100

    teclado

---
+++ src/chip8_input.h
@@ -0,0 +1,17 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+// Mapea las teclas del teclado de la máquina
+void capturarTeclado();
+
+// Verifica si una tecla está presionada
+bool estaPresionada(uint8_t key);
+
+// Devuelve las teclas presionadas
+uint8_t *getTeclado();
+
+#endif // !INPUT
