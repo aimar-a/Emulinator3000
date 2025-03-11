5cc8cced3cd20764736c4d7f872e21321ff543f1
malbisudlf <mikel.albisudela@opendeusto.es>
2025-03-11 14:30:21 +0100

    Settings lee las roms

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include "chip8_cpu.h"
 
+#define SDL_MAIN_HANDLED // Evita que SDL2 defina su propia función main
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h> // Si usas texto en la UI
+
 int main(int argc, char *argv[])
 {
   chip8cpuLaunch("../resources/chip8-roms/games/Space Invaders [David Winter].ch8");
