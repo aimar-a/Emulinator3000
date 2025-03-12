dc5be4f175a1a4fe778d75355919abdd7808b556
aimar <aimar.a@opendeusto.es>
2025-03-12 09:34:37 +0100

    fix directorios

--- src/main.c
+++ src/main.c
@@ -4,6 +4,15 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h> // Si usas texto en la UI
 
+/*
+
+PARA EJECUTAR EL CODIGO: (dentro de la consola MinGW32)
+
+make
+bin/chip8_emulator.exe
+
+*/
+
 int main(int argc, char *argv[])
 {
   showInitialWindow();
