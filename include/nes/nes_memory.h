0bfdb57936d17e3a9486339abb0081801b68131e
malbisudlf <mikel.albisudela@opendeusto.es>
2025-03-14 11:52:49 +0100

    Aun da error pero avance con los mappers

--- include/nes/nes_memory.h
+++ include/nes/nes_memory.h
@@ -6,4 +6,24 @@
 uint8_t nes_read(NES *nes, uint16_t address);
 void nes_write(NES *nes, uint16_t address, uint8_t value);
 
+void init_nrom(NES *nes);
+uint8_t read_nrom(NES *nes, uint16_t address);
+void write_nrom(NES *nes, uint16_t address, uint8_t value);
+
+void init_mmc1(NES *nes);
+uint8_t read_mmc1(NES *nes, uint16_t address);
+void write_mmc1(NES *nes, uint16_t address, uint8_t value);
+
+void init_unrom(NES *nes);
+uint8_t read_unrom(NES *nes, uint16_t address);
+void write_unrom(NES *nes, uint16_t address, uint8_t value);
+
+void init_cnrom(NES *nes);
+uint8_t read_cnrom(NES *nes, uint16_t address);
+void write_cnrom(NES *nes, uint16_t address, uint8_t value);
+
+void init_mmc3(NES *nes);
+uint8_t read_mmc3(NES *nes, uint16_t address);
+void write_mmc3(NES *nes, uint16_t address, uint8_t value);
+
 #endif
